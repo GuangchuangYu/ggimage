0ec6687167a696214af7afdebc3983da18107fb0
guangchuang yu <guangchuangyu@gamil.com>
2017-02-22 13:03:42 +0800

    image parameter

--- R/geom_image.R
+++ R/geom_image.R
@@ -29,8 +29,6 @@ geom_image <- function(mapping=NULL, data=NULL, stat="identity",
                        position="identity", inherit.aes=TRUE,
                        na.rm=FALSE, by="width", ...) {
 
-    dots <- eval(substitute(alist(...)))
-    image <- dots$image
     by <- match.arg(by, c("width", "height"))
 
     layer(
@@ -43,12 +41,12 @@ geom_image <- function(mapping=NULL, data=NULL, stat="identity",
         inherit.aes=inherit.aes,
         params = list(
             na.rm = na.rm,
-            image = image,
             by = by,
             ...),
         check.aes = FALSE
     )
 }
+
 
 ##' @importFrom ggplot2 ggproto
 ##' @importFrom ggplot2 Geom
@@ -57,13 +55,10 @@ geom_image <- function(mapping=NULL, data=NULL, stat="identity",
 ##' @importFrom grid gTree
 ##' @importFrom grid gList
 GeomImage <- ggproto("GeomImage", Geom,
-                     draw_panel = function(data, panel_scales, coord, image, by, na.rm=FALSE) {
+                     draw_panel = function(data, panel_scales, coord, by, na.rm=FALSE) {
                          data <- coord$transform(data, panel_scales)
-                         if (!is.null(data$image)) {
-                             image <- data$image
-                         }
 
-                         groups <- split(data, factor(image))
+                         groups <- split(data, factor(data$image))
                          imgs <- names(groups)
                          grobs <- lapply(seq_along(groups), function(i) {
                              data <- groups[[i]]
@@ -72,10 +67,10 @@ GeomImage <- ggproto("GeomImage", Geom,
                          ggplot2:::ggname("geom_image",
                                           gTree(children = do.call("gList", grobs)))
                      },
-                     non_missing_aes = c("size"),
+                     non_missing_aes = c("size", "image"),
                      required_aes = c("x", "y"),
-                     default_aes = aes(size=0.05),
-                     draw_key = draw_key_blank
+                     default_aes = aes(size=0.05, image="https://www.r-project.org/logo/Rlogo.png"),
+                     draw_key = draw_key_blank ## need to write the `draw_key_image` function.
                      )
 
 ##' @importFrom EBImage readImage
@@ -99,7 +94,8 @@ imageGrob <- function(x, y, size, img, by) {
                image = img,
                default.units = "native",
                height = height,
-               width = width)
+               width = width,
+               interpolate = FALSE)
 }
 
 
@@ -108,3 +104,79 @@ getAR <- function(img) {
     dims[1]/dims[2]
 }
 
+
+##################################################
+##                                              ##
+## another solution, but the speed is too slow  ##
+##                                              ##
+##################################################
+
+## draw_key_image <- function(data, params, size) {
+##     imageGrob(0.5, 0.5, image=data$image, size=data$size)
+## }
+
+
+## ##' @importFrom ggplot2 ggproto
+## ##' @importFrom ggplot2 Geom
+## ##' @importFrom ggplot2 aes
+## ##' @importFrom ggplot2 draw_key_blank
+## GeomImage <- ggproto("GeomImage", Geom,
+##                      non_missing_aes = c("size", "image"),
+##                      required_aes = c("x", "y"),
+##                      default_aes = aes(size=0.05, image="https://www.r-project.org/logo/Rlogo.png"),
+##                      draw_panel = function(data, panel_scales, coord, by, na.rm=FALSE) {
+##                          data$image <- as.character(data$image)
+##                          data <- coord$transform(data, panel_scales)
+##                          imageGrob(data$x, data$y, data$image, data$size, by)
+##                      },
+##                      draw_key = draw_key_image
+##                      )
+
+
+## ##' @importFrom grid grob
+## imageGrob <- function(x, y, image, size=0.05, by="width") {
+##     grob(x=x, y=y, image=image, size=size, by=by, cl="image")
+## }
+
+## ##' @importFrom grid drawDetails
+## ##' @importFrom grid grid.raster
+## ##' @importFrom EBImage readImage
+## ##' @method drawDetails image
+## ##' @export
+## drawDetails.image <- function(x, recording=FALSE) {
+##     image_object <- lapply(x$image, readImage)
+##     names(image_object) <- x$image
+##     for (i in seq_along(x$image)) {
+##         img <- image_object[[x$image[i]]]
+##         size <- x$size[i]
+##         by <- x$by
+##         asp <- getAR(img)
+##         if (is.na(size)) {
+##             width <- NULL
+##             height <- NULL
+##         } else if (by == "width") {
+##             width <- size
+##             height <- size/asp
+##         } else {
+##             width <- size * asp
+##             height <- size
+##         }
+
+##         grid.raster(x$x[i], x$y[i],
+##                     width = width,
+##                     height = height,
+##                     image = img,
+##                     interpolate=FALSE)
+##     }
+## }
+
+## ##' @importFrom ggplot2 discrete_scale
+## ##' @importFrom scales identity_pal
+## ##' @importFrom ggplot2 ScaleDiscreteIdentity
+## ##' @export
+## scale_image <- function(..., guide = "legend") {
+##   sc <- discrete_scale("image", "identity", identity_pal(), ..., guide = guide,
+##                        super = ScaleDiscreteIdentity)
+
+##   sc
+## }
