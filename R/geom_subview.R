bebc9ab24bac94d82e1aea1640e56115fd31938b
guangchuang yu <guangchuangyu@gamil.com>
2017-11-21 13:09:32 +0800

    better geom_subview

--- R/geom_subview.R
+++ R/geom_subview.R
@@ -2,57 +2,85 @@
 ##'
 ##'
 ##' @title geom_subview
-##' @param subview subview
-##' @param x x position
-##' @param y y position
+##' @param mapping aes mapping, requires 'x', 'y' and 'subview'
+##' @param data data frame
 ##' @param width width
 ##' @param height height
+##' @param x x position of subview. This parameter works if mapping and data is not provided
+##' @param y y position of subview. This parameter works if mapping and data is not provided
+##' @param subview subview to plot, if not provided in data and specify by mapping
 ##' @return layer
 ##' @importFrom ggplot2 annotation_custom
+##' @importFrom ggplot2 aes_
+##' @importFrom tibble as_data_frame
 ## @importFrom grid convertUnit
 ## @importFrom grid viewport
 ## @importFrom grid pushViewport
 ##' @importFrom tibble data_frame
 ##' @export
 ##' @author guangchuang yu
-geom_subview <- function(subview, x, y, width=.1, height=.1) {
-    len <- sapply(list(subview, x, y, width, height), length)
-    mlen <- max(len)
-    for (i in len) {
-        if (mlen %% i != 0)
-            stop("subview, x, y, width and height should have equal length")
+geom_subview <- function(mapping = NULL, data = NULL, width=.1, height=.1, x = NULL, y = NULL, subview = NULL) {
+    ## can't support `aes(x, y, subview=subview)` as ggplot2 will throw:
+    ##     cannot coerce class "c("ggtree", "gg", "ggplot")" to a data.frame
+
+    ## this is a hack to support `aes` without `inherit.aes`. mapping and data must be provided.
+
+    if (is.null(data)) {
+        data <- data_frame(x = x, y = y)
+    } else if (!inherits(data, 'tbl')) {
+        data <- as_data_frame(data)
     }
-    if (!inherits(subview, "list")) {
-        subview <- list(subview)
+
+    if (is.null(mapping)) {
+        mapping <- aes_(x = ~x, y = ~y)
+    }
+    mapping <- as.list(mapping)
+    if (is.null(mapping$x)) {
+        stop("x aesthetic mapping should be provided")
+    }
+    if (is.null(mapping$y)) {
+        stop("y aesthetic mapping should be provided")
+    }
+    if (is.null(mapping$subview) && is.null(subview)) {
+        stop("subview must be provided")
+    }
+    if (is.null(mapping$subview)) {
+        if (!inherits(subview, "list")) {
+            subview <- list(subview)
+        }
+        data$subview <- subview
+    } else {
+        data$subview <- data[[as.character(mapping$subview)]]
     }
 
-    d <- data_frame(x=x, y=y,
-                    width=width,
-                    height=height,
-                    subview=subview)
-    d$xmin <- d$x - d$width/2
-    d$xmax <- d$x + d$width/2
-    d$ymin <- d$y - d$height/2
-    d$ymax <- d$y + d$height/2
+    xvar <- as.character(mapping$x)
+    yvar <- as.character(mapping$y)
 
-    lapply(1:nrow(d), function(i) {
-        ## x <- d$x[i]
-        ## y <- d$y[i]
-        ## width <- d$width[i]
-        ## height <- d$height[i]
+    if (is.null(mapping$width)) {
+        data$width <- width
+    } else {
+        data$width <- data[[as.character(mapping$width)]]
+    }
 
-        ## pushViewport(viewport())
-        ## xmin <- convertUnit(unit(x, "native") - unit(width/2, "npc"), "native")
-        ## xmax <- convertUnit(unit(x, "native") + unit(width/2, "npc"), "native")
-        ## ymin <- convertUnit(unit(y, "native") - unit(height/2, "npc"), "native")
-        ## ymax <- convertUnit(unit(y, "native") + unit(height/2, "npc"), "native")
+    if (is.null(mapping$height)) {
+        data$height <- height
+    } else {
+        data$height <- data[[as.character(mapping$height)]]
+    }
 
+
+    data$xmin <- data[[xvar]] - data$width/2
+    data$xmax <- data[[xvar]] + data$width/2
+    data$ymin <- data[[yvar]] - data$height/2
+    data$ymax <- data[[yvar]] + data$height/2
+
+    lapply(1:nrow(data), function(i) {
         annotation_custom(
-            toGrob(d$subview[[i]]),
-            xmin = d$xmin[i],
-            xmax = d$xmax[i],
-            ymin = d$ymin[i],
-            ymax = d$ymax[i])
+            toGrob(data$subview[[i]]),
+            xmin = data$xmin[i],
+            xmax = data$xmax[i],
+            ymin = data$ymin[i],
+            ymax = data$ymax[i])
     })
 }
 
@@ -94,10 +122,3 @@ toGrob_ <- function(subview) {
 
 unit <- grid::unit
 
-
-
-##' @method length gg
-##' @export
-length.gg <- function(x) {
-    return(1)
-}
