bbf04923f89fa821f0a5ec53897b7771b414ea96
Guangchuang Yu <guangchuangyu@gmail.com>
2022-12-02 14:25:06 +0800

    compatible with last-plot

--- R/ggpreview.R
+++ R/ggpreview.R
@@ -9,17 +9,22 @@
 ##' @return a preview of the figure
 ##' @importFrom ggplot2 last_plot
 ##' @importFrom ggplot2 ggsave
-##' @importFrom ggplotify as.ggplot
+##' @importFrom ggplotify as.grob
+##' @importFrom grid grid.draw
+##' @importFrom grid grid.newpage
 ##' @importFrom magick image_read
 ##' @export
 ##' @author Guangchuang Yu
 ggpreview <- function(plot = last_plot(), width, height, units = 'in') {
     f <- tempfile(fileext = '.png') 
     if (!inherits(plot, 'gg')) {
-        plot <- as.ggplot(plot)    
+        ## plot <- as.ggplot(plot)    
+        plot <- as.grob(plot)    
     }
 
     ggsave(filename = f, plot, width=width, height=height, units=units)
-    as.ggplot(image_read(f))
+    ## as.ggplot(image_read(f))
+    grid.newpage()
+    grid.draw(as.grob(image_read(f)))
 }
 
